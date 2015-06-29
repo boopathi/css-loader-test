fa67b77c09569a280fb1bf374d2a601d0008e601
Boopathi Rajaa <me@boopathi.in>
2015-06-29 15:05:31 +0530

    Adds webpack concigs

--- 1.webpack.config.js
+++ 1.webpack.config.js
@@ -1,6 +1,7 @@
 var Nested = require('postcss-nested');
 var Mixins = require('postcss-mixins');
 var SimpleVars = require('postcss-simple-vars');
+var cssnext = require('cssnext');
 
 module.exports = {
 	entry: './index.js',
@@ -8,15 +9,13 @@ module.exports = {
 		path: 'public',
 		filename: 'bundle.js'
 	},
-	cssnext: {
-		plugins: [ SimpleVars, Mixins, Nested ]
-	},
+ 	postcss: [ cssnext(), Nested, SimpleVars, Mixins],
 	module: {
 		loaders: [
 			{
 				test: /\.css$/,
 				// loader: 'style?singleton!css?module&sourceMap!cssnext'
-				loader: 'cssnext'
+				loader: 'postcss'
 			}
 		]
 	}
---
+++ README.md
@@ -0,0 +1,7 @@
+# css-loader-test
+
+```
+`npm bin`/webpack --config 1.webpack.config.js
+
+`npm bin`/webpack --config 2.webpack.config.js
+```
